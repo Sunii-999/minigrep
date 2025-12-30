ba27be79fc82ff2b181cf213f4dde0817a309983
Sunii <stijnwal01@gmail.com>
2025-12-30 15:07:36 +0100

    add: Added functionality with test driven development and worked with environment variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
+
+use minigrep::{search, search_case_insensitive};
+
 
 fn main() {
 
@@ -11,23 +15,38 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    println!("============================");
+    println!("Searching for: {}", config.query);
+    println!("Insilde file: {}", config.file_path);
+    println!("============================");
 
-    run(config);
+    if let Err(e)= run(config){
+        println!("Application error: {e}");
+        process::exit(1)
+    }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
-struct Config {
-    query: String,
-    file_path: String,
-}
 
 impl Config {
     fn build(args: &[String]) -> Result<Config, &'static str> {
@@ -38,6 +57,9 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
